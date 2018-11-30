343d9c3bda3312cb49144063ae093616ceec5355
Phillip Carter <phcart@microsoft.com>
2018-11-29 16:10:47 -0800

    Initial commit

---
+++ UserTest/Fruit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp68
+{
+    public class Fruit
+    {
+        public Fruit(string name, bool hasSeeds)
+        {
+            Name = name;
+            HasSeeds = hasSeeds;
+        }
+
+        public string Name { get; set; }
+
+        public bool HasSeeds { get; set; }
+    }
+}
---
+++ UserTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ConsoleApp68
+{
+    class Program
+    {
+        static List<Person> GetFruits(string json)
+        {
+            throw new NotImplementedException();
+        }
+
+        static List<Fruit> GetFruit(string json)
+        {
+            throw new NotImplementedException();
+        }
+
+        static string ReadFile(string path)
+        {
+            // Consider using File.ReadAllText
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            var fruitPath = @"..\..\..\fruit.json";
+            var peoplePath = @"..\..\..\people.json";
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
