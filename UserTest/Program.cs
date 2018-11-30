9d799040728b01de52a6401534dd986c383f8399
Phillip Carter <phcart@microsoft.com>
2018-11-29 16:16:58 -0800

    Clarify printing stats about the list of thigns

--- UserTest/Program.cs
+++ UserTest/Program.cs
@@ -6,13 +6,20 @@ namespace ConsoleApp68
 {
     class Program
     {
+        static void PrintStats(List<Fruit> fruit, List<Person> people)
+        {
+            // Print out the values of properties in fruit and people
+        }
+
         static List<Person> GetFruits(string json)
         {
+            // Use JSON.NET to convert to JSON string into a list of people
             throw new NotImplementedException();
         }
 
         static List<Fruit> GetFruit(string json)
         {
+            // Use JSON.NET to convert the JSON string into a list of fruits
             throw new NotImplementedException();
         }
 
@@ -27,7 +34,9 @@ namespace ConsoleApp68
             var fruitPath = @"..\..\..\fruit.json";
             var peoplePath = @"..\..\..\people.json";
 
-
+            // 1. Read the JSON files
+            // 2. Convert them int fruits and people (using above methods)
+            // 3. Call the method to print stats about the fruit and people
 
             Console.ReadKey();
         }
